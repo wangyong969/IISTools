9b2764baa3d69e1516e5c9232cb1186a50b4c5dc
wangyong969 <35441214@qq.com>
2016-11-18 13:43:01 +0800

    Revert "Revert "No.1""
    This reverts commit d804579a5c9af53115802b2b8c740483e076843a.

---
+++ CustomException.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace OneV.IISTools
+{
+    public class CustomException : Exception
+    {
+        public int Code { get; set; }
+        public string Msg { get; set; }
+        public CustomException(int code, string msg)
+            : base(msg)
+        {
+            this.Code = code;
+            this.Msg = msg;
+        }
+        public CustomException(string msg)
+            : base(msg)
+        {
+            this.Code = -2;
+            this.Msg = msg;
+        }
+    }
+
+}
